b2ce88492102e471e77c15858ec6dfb3272b764e
Pham Hung <hungpham3050@gmail.com>
2024-12-22 20:48:04 +0700

    Add ThongkeVM

--- CinemaManagement/ViewModel/AdminVM/ThongKeVM/ThongKeKHVM.cs
+++ CinemaManagement/ViewModel/AdminVM/ThongKeVM/ThongKeKHVM.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CinemaManagement.DTOs;
+﻿using CinemaManagement.DTOs;
 using CinemaManagement.Models.DAL;
 using LiveCharts;
 using LiveCharts.Wpf;
